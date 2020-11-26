de5330eed7aabc40ce77670015fab50c2ba079bf
Artivain <thomas@artivain.tk>
2020-11-25 22:20:28 -0500

    Simplification du JS

--- textes/script.js
+++ textes/script.js
@@ -1,15 +1,15 @@
+// --- Animation 1 ---
 // Wrap every letter in a span
 var textWrapper = document.querySelector('.anim1');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({ loop: false })
-	.add({
-		targets: '.anim1 .letter',
-		translateY: ["1.1em", 0],
-		translateX: ["0.55em", 0],
-		translateZ: 0,
-		rotateZ: [180, 0],
-		duration: 750,
-		easing: "easeOutExpo",
-		delay: (el, i) => 50 * i
-	});
+anime.timeline({ loop: false }).add({
+	targets: '.anim1 .letter',
+	translateY: ["1.1em", 0],
+	translateX: ["0.55em", 0],
+	translateZ: 0,
+	rotateZ: [180, 0],
+	duration: 750,
+	easing: "easeOutExpo",
+	delay: (el, i) => 50 * i
+});
